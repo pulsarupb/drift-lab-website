a50413e160a23a71ab60faf88ddddc00213d4788
lazar <dasidasi86@gmail.com>
2026-05-07 21:51:40 +0300

    feat: add SponsorsSection component and sponsors data

---
+++ src/data/sponsors/index.ts
@@ -0,0 +1,64 @@
+export interface Sponsor {
+  id: string
+  name: string
+  category?: string
+  description?: string
+  websiteUrl?: string
+  logoUrl?: string
+}
+
+const sponsors: Sponsor[] = [
+  {
+    id: "purequad",
+    name: "PureQuad",
+    category: "Money",
+    websiteUrl: "https://www.purequad.com/",
+  },
+  {
+    id: "fimm",
+    name: "FIMM",
+    category: "Academic",
+    websiteUrl: "http://www.mecanica.pub.ro/new/",
+  },
+  {
+    id: "tech-con",
+    name: "Tech-Con",
+    category: "Products",
+    websiteUrl: "https://tech-con.ro/en/",
+  },
+  {
+    id: "nxp",
+    name: "NXP",
+    category: "Products",
+    websiteUrl: "https://www.nxp.com/",
+  },
+  {
+    id: "kiwisolar",
+    name: "kiwisolar",
+    category: "Products",
+    websiteUrl: "https://kiwisolar.ro/",
+  },
+  {
+    id: "mouser",
+    name: "Mouser",
+    category: "Discount",
+    websiteUrl: "https://ro.mouser.com/",
+  },
+  {
+    id: "campus",
+    name: "CAMPUS",
+    category: "Academic",
+  },
+  {
+    id: "aft",
+    name: "AFT",
+    category: "Technical",
+  },
+  {
+    id: "agora-robotics",
+    name: "Agora Robotics",
+    category: "Technical",
+  },
+]
+
+export default sponsors
